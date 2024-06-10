3f956725fb2daadb8bff9debee0337a5a519735b
Александр Васянькин <henchfiend@yandex.ru>
2024-06-10 22:02:35 +0300

    форма DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @MinLength(2, { message: 'Имя не может быть короче 2 символов' })
+  @MaxLength(30, { message: 'Имя не может быть длиннее 30 символов' })
+  username: string;
+
+  @MinLength(2, { message: 'Описание не может быть короче 2 символов' })
+  @MaxLength(200, { message: 'Описание не может быть длиннее 200 символов' })
+  @IsOptional()
+  about: string;
+
+  @IsUrl()
+  @IsOptional()
+  avatar: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
